63ef3f2319a4179cde9b6828371c9ca212d8d505
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-02 18:18:00 +0900

    file tree and dotkun

---
+++ Dotkun/Model/Dotkun.swift
@@ -0,0 +1,21 @@
+//
+//  Dotkun.swift
+//  Dotkun
+//
+//  Created by SakuragiYoshimasa on 2015/11/02.
+//  Copyright © 2015年 SakuragiYoshimasa. All rights reserved.
+import UIKit
+
+class Dotkun {
+    private var index: Int! = nil
+    private var color: UIColor! = nil
+    private var colorType: ColorType! = nil
+    private var position: CGPoint! = nil
+    private var power: Int! = nil
+    private var hp: Int! = nil
+    private var speed: CGFloat! = nil
+}
+
+enum ColorType {
+    
+}
