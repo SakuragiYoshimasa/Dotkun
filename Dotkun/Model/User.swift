904bf4cccff4f28904538a00c973ba447511b27e
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-02 18:20:07 +0900

    add userClass

---
+++ Dotkun/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  Dotkun
+//
+//  Created by SakuragiYoshimasa on 2015/11/02.
+//  Copyright © 2015年 SakuragiYoshimasa. All rights reserved.
+//
+
+class User {
+    private var userId: String! = nil
+    private var userName: String! = nil
+}
