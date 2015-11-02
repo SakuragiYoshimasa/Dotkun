58d30419d36531293ad0478f5f8f6186b3c794f4
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-02 17:48:39 +0900

    Initial Commit

---
+++ Dotkun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dotkun
+//
+//  Created by SakuragiYoshimasa on 2015/11/02.
+//  Copyright © 2015年 SakuragiYoshimasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
