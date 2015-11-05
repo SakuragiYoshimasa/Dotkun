51dd76daf5626c90d18e88ea9bb852ee7e1f3bf3
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-05 18:45:28 +0900

    ファイル作成

---
+++ Dotkun/ViewController/SelectGameViewController/GameViewController/GameController.swift
@@ -0,0 +1,11 @@
+//
+//  GameController.swift
+//  Dotkun
+//
+//  Created by SakuragiYoshimasa on 2015/11/05.
+//  Copyright © 2015年 SakuragiYoshimasa. All rights reserved.
+//
+
+class GameController {
+    
+}
---
+++ Dotkun/ViewController/SelectGameViewController/GameViewController/GameUtils.swift
@@ -0,0 +1,10 @@
+//
+//  GameUtils.swift
+//  Dotkun
+//
+//  Created by SakuragiYoshimasa on 2015/11/05.
+//  Copyright © 2015年 SakuragiYoshimasa. All rights reserved.
+//
+class GameUtils {
+
+}
