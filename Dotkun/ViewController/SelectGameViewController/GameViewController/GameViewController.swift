30255f1dac9a793cf933a2881177c7807a9c4c7a
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-07 16:53:06 +0900

    不要箇所の消去

--- Dotkun/ViewController/SelectGameViewController/GameViewController/GameViewController.swift
+++ Dotkun/ViewController/SelectGameViewController/GameViewController/GameViewController.swift
@@ -14,7 +14,6 @@ class GameViewController: BaseViewController {
     var gameView: GameView! = nil
     var gameController: GameController! = nil
     var startButton: UIButton! = nil
-    //var dotkuns: [Dotkun] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,34 +40,12 @@ class GameViewController: BaseViewController {
             startButton.backgroundColor = Constants.BACKCOLOR
             self.view.addSubview(startButton)
         }
-        
-       // initGame()
-    }
-    
-    //func initGame() {
-        /*dotkuns = []
-        for i in 0...2047 {
-            let dotkun = Dotkun(color: TestUtil.randomColor(), pos: TestUtil.randomPoint(gameView.bounds))
-            dotkun.setIndex(i)
-            dotkuns.append(dotkun)
-            gameView.addObject(dotkun)
-            gameController.setDotkun(dotkun, index: i)
-        }
-        */
-   // }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func onUpdate() {
         gameController.update()
         gameView.setNeedsDisplay()
         startButton.setNeedsDisplay()
-       /* for dotkun in dotkuns {
-            dotkun.move(Util.generateRandom()*2-1, y: Util.generateRandom()*2-1)
-        }*/
     }
     
     func startGame(){
