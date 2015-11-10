7836cc052d4407bcf4a8c5d9a3522e28d0c08da5
ha1fha1f <half.ldk.hiz@gmail.com>
2015-11-10 15:07:30 +0900

    add fabric, twitter login

--- Dotkun/ViewController/LoginViewController.swift
+++ Dotkun/ViewController/LoginViewController.swift
@@ -7,100 +7,93 @@
 //
 
 import UIKit
+import TwitterKit
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
-    
-    // 設定項目
-    let borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
-    let boxOffsetY: CGFloat = 200
-    let boxOffsetY2: CGFloat = 330
+class LoginViewController: UIViewController {
     
     //logo
     let logoLabel = UILabel()
     
-    // for pattern1
-    let borderView: UIView = UIView()
-    let idField = UITextField()
-    let passField = UITextField()
+    let indicator = UIActivityIndicatorView(frame: CGRectMake(0,0,50,50))
     
+    let logInButton = UIButton()
+    
+    override func viewWillAppear(animated: Bool) {
+        tryLogin()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ModelManager.manager.setAccount(User())
-        
-        
-        let boxWidth = self.view.frame.width - 50
-        let boxHeight: CGFloat = 50
-        
-        
-        pattern1(boxWidth, boxHeight: boxHeight)
-        
-        // tapGesture
-        let gestureTap = UITapGestureRecognizer(target: self, action: "resignFirstResponderAll")
-        self.view.addGestureRecognizer(gestureTap)
-        
         // logo
-        logoLabel.frame = CGRectMake(50, boxOffsetY-130, self.view.frame.width-100, 100)
+        logoLabel.frame = CGRectMake(50, 50, self.view.frame.width-100, 100)
         logoLabel.text = "LOGO"
         logoLabel.font = UIFont.systemFontOfSize(80)
         logoLabel.textAlignment = NSTextAlignment.Center
         logoLabel.textColor = UIColor.blackColor()
         self.view.addSubview(logoLabel)
+        
+        logInButton.frame = CGRect(origin: CGPoint.zero, size: CGSizeMake(100,50))
+        logInButton.backgroundColor = UIColor.cyanColor()
+        logInButton.center = self.view.center
+        logInButton.addTarget(self, action: "tryLogin", forControlEvents: .TouchUpInside)
+        self.view.addSubview(logInButton)
+        logInButton.hidden = true
+        
+        indicator.hidesWhenStopped = true
+        indicator.activityIndicatorViewStyle = .Gray
+        indicator.layer.position = self.view.center
+        self.view.addSubview(indicator)
+        
     }
     
-    func pattern1(boxWidth: CGFloat, boxHeight: CGFloat) {
-        borderView.frame = CGRectMake((self.view.frame.width - boxWidth)/2, boxOffsetY, boxWidth, boxHeight * 2)
-        borderView.layer.borderColor = borderColor.CGColor
-        borderView.layer.borderWidth = 1.0
-        borderView.layer.cornerRadius = 5.0
-        borderView.layer.masksToBounds = true
-        //中央線
-        UIGraphicsBeginImageContextWithOptions(borderView.frame.size, false, 0)
-        let path = UIBezierPath()
-        path.moveToPoint(CGPointMake(0, boxHeight));
-        path.addLineToPoint(CGPointMake(boxWidth, boxHeight))
-        borderColor.setStroke()
-        path.lineWidth = 1.0
-        path.stroke()
-        borderView.layer.contents = UIGraphicsGetImageFromCurrentImageContext().CGImage
-        UIGraphicsEndImageContext()
-        self.view.addSubview(borderView)
+    func tryLogin() {
+        indicator.startAnimating()
+        logInButton.userInteractionEnabled = false
         
-        idField.frame = CGRectMake(7, 0, boxWidth-10, boxHeight)
-        idField.placeholder = "ユーザーID"
-        idField.borderStyle = UITextBorderStyle.None
-        idField.returnKeyType = UIReturnKeyType.Done
-        idField.delegate = self
-        borderView.addSubview(idField)
+        let loginCompletion: TWTRLogInCompletion = { (session, error) in
+            if let s = session {
+                print(s.userName)
+                self.onLoggedIn(s.userID)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription)
+                self.indicator.stopAnimating()
+                self.logInButton.hidden = false
+                self.logInButton.userInteractionEnabled = true
+            }
+        }
         
-        passField.frame = CGRectMake(7, boxHeight, boxWidth-10, boxHeight)
-        passField.placeholder = "パスワード"
-        passField.borderStyle = UITextBorderStyle.None
-        passField.returnKeyType = UIReturnKeyType.Done
-        passField.secureTextEntry = true
-        passField.delegate = self
-        borderView.addSubview(passField)
+        // すでにtokenとかが存在
+        if let session = Twitter.sharedInstance().sessionStore.session() {
+            Twitter.sharedInstance().logInWithExistingAuthToken(session.authToken, authTokenSecret: session.authTokenSecret, completion: loginCompletion)
+        // tokenなし
+        } else {
+            Twitter.sharedInstance().logInWithCompletion(loginCompletion)
+        }
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool{
-        textField.resignFirstResponder()
-        return true
+    func onLoggedIn(userId: String) {
+        let client = TWTRAPIClient(userID: userId)
+        
+        print("client: \(client.userID)")
+
+        client.loadUserWithID(userId) { user, error in
+            self.indicator.stopAnimating()
+            if let u = user {
+                print(u.profileImageURL)
+                ModelManager.manager.setAccount(User(tUser: u))
+                self.performSegueWithIdentifier("loggedIn", sender: nil)
+            } else {
+                print("login Error!")
+                self.logInButton.hidden = false
+                self.logInButton.userInteractionEnabled = true
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    // ここで閉じるとかっこいい
-    override func viewWillDisappear(animated: Bool) {
-        resignFirstResponderAll()
-    }
-    
-    func resignFirstResponderAll() {
-        idField.resignFirstResponder()
-        passField.resignFirstResponder()
     }
     
 }
--- Podfile
+++ Podfile
@@ -2,3 +2,6 @@ platform :ios, '8.0'
 use_frameworks!
 
 pod 'RealmSwift'
+pod 'Fabric'
+pod 'TwitterKit'
+pod 'TwitterCore'
